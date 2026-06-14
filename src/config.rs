582e18b9acdd79dce7861e0b3fd324c2521c6b0f
Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>
2026-06-14 22:10:01 +0530

    ci(release): Add a release CI
    Signed-off-by: Vaishnav Sabari Girish <vaishnav.sabari.girish@gmail.com>

--- src/config.rs
+++ src/config.rs
@@ -134,7 +134,7 @@ impl Default for Config {
 #[derive(Parser)]
 #[command(
     name = "comchan",
-    version = "0.10.0",
+    version = "0.10.1",
     author = "Vaishnav-Sabari-Girish",
     about = "Blazingly Fast Minimal Serial Monitor with Plotting"
 )]
